c83c83a49ac26e252d45cace1a9358f14819ddab
jkholmatov <jkholmatov@yandex.com>
2019-01-07 18:41:44 +0500

    test

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -8,6 +8,6 @@
   </div>
 </div>
 <div class="page-content">
-    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Vitae eos sapiente dignissimos beatae nihil ipsum cupiditate tempora praesentium, quia similique! Ducimus hic voluptate, animi omnis voluptatum quos veritatis repellendus vel.</p>
+  dashboard
 </div>
 @endsection
